1b938f16a9e3393d62b4eddd3e609d5dc835fe17
Dineva Ivana <92163599+dinevai@users.noreply.github.com>
2021-11-17 00:01:31 +0100

    Add files via upload

--- Proekt-Dinamicki del/javascript/script.js
+++ Proekt-Dinamicki del/javascript/script.js
@@ -102,3 +102,21 @@ function clickCounter() {
   }
 }
 
+function onc(){
+    like();
+    clickCounter();
+}
+function clickCounterMinus(){
+    if(typeof(Storage) !== "undefined"){
+        if(localStorage.clickcount){
+            localStorage.clickcount = Number(localStorage.clickcount)-1;
+        } 
+        else{
+            localStorage.clickcount = 1;
+        }
+        document.getElementById("result").innerHTML = localStorage.clickcount;
+    }
+    else{
+        document.getElementById("result").innerHTML = "";
+    }
+}
